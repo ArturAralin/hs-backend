7d867f180ce47c8d17f652193a6ebb86edc55992
Artur Aralin <artur.aralin97@gmail.com>
2020-04-01 17:22:36 +0300

    update deps

--- project.clj
+++ project.clj
@@ -3,24 +3,23 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [environ "1.1.0"]
                  [http-kit "2.3.0"]
                  [ring/ring-json "0.5.0"]
                  [ring "1.7.0"]
                  [compojure "1.6.1"]
-                 [funcool/struct "1.4.0"]
                  [honeysql "0.9.10"]
                  [migratus "1.2.0"]
                  [org.postgresql/postgresql "42.1.4"]
-                 [org.clojure/java.jdbc "0.7.11"]]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [nilenso/honeysql-postgres "0.2.6"]]
   :plugins [[lein-environ "1.1.0"]]
   :main ^:skip-aot health-samurai-back-end.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:env {:environment "development"
-                         :app-port 8080
+                         :app-port "8080"
                          :database-type "postgresql"}}
              :dev-local {:env {
                                :database-name "health_samurai_app"
