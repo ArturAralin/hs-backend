2e64329e81851287c62dd49a3b6727f478c6704f
Artur Aralin <artur.aralin97@gmail.com>
2020-04-01 17:22:05 +0300

    update init schema

--- resources/migrations/init.sql
+++ resources/migrations/init.sql
@@ -2,9 +2,9 @@ CREATE TABLE "patients" (
   id SERIAL PRIMARY KEY,
   fio TEXT not null,
   gender TEXT NOT NULL,
-  birthday DATE NOT NULL,
-  address TEXT NOT NULL,
+  birthday DATE,
+  address TEXT NOT NULL DEFAULT '',
   oms_policy TEXT NOT NULL,
   created_at TIMESTAMPTZ DEFAULT NOW(),
-  updated_at TIMESTAMPTZ DEFAULT NOW()
+  updated_at TIMESTAMPTZ
 );
