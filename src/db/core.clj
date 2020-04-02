29d928f0954f201a1afa8045e63b418203907146
Artur Aralin <artur.aralin97@gmail.com>
2020-04-02 15:13:58 +0300

    wrap raw! with try-catch

--- src/db/core.clj
+++ src/db/core.clj
@@ -4,15 +4,24 @@
    [environ.core :refer [env]]
    [honeysql.core :as sql]))
 
-(def db-config {:dbtype (:database-type env)
-            :dbname (:database-name env)
-            :host (:database-host env)
-            :user (:database-username env)
-            :password (:database-password env)
-            :port (:database-port env)})
+(def db-config
+  {:dbtype (:database-type env)
+   :dbname (:database-name env)
+   :host (:database-host env)
+   :user (:database-username env)
+   :password (:database-password env)
+   :port (:database-port env)})
 
+;; exec raw JDBC query
 (defn raw! [query]
-  (io! (jdbc/query db-config query)))
+  (try
+    (io! (jdbc/query db-config query))
+    (catch org.postgresql.util.PSQLException e
+      (let [code (.getErrorCode e)]
+        (if (= code 0)
+          '()
+          (throw e))))))
 
+;; exec honeysql query
 (defn q! [query]
   (raw! (sql/format query)))
