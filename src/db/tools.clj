8bdc2043eb0ab682bd66a591b9c15bed3af789af
Artur Aralin <artur.aralin97@gmail.com>
2020-04-02 15:14:12 +0300

    add new tools

--- src/db/tools.clj
+++ src/db/tools.clj
@@ -4,3 +4,19 @@
 ;; convert ISO8601 to LocalDate
 (defn parse-date [string]
   (LocalDate/parse string))
+
+(defn resolve-nulls [row]
+  (->> row
+       (filter (fn [[_ v]] (some? v)))
+       (map (fn [[k v]] [k (if (= v :null) nil v)]))
+       (into {})))
+
+(defn nil-to-null-keyword [row]
+  (->> row
+       (map (fn [[k v]] [k (if (nil? v) :null v)]))
+       (into {})))
+
+(defn try-int [v]
+  (try
+    (Integer. v)
+    (catch Exception _ nil)))
